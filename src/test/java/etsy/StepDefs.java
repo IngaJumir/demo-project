bcd4e1817e0ebfc7c443b85c53cd4f03e84836ff
IngaJumir <ingajumir@gmail.com>
2024-02-23 19:02:00 -0600

    step def update

--- src/test/java/etsy/StepDefs.java
+++ src/test/java/etsy/StepDefs.java
@@ -12,37 +12,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 import java.util.concurrent.TimeUnit;
 
 
 public class StepDefs {
-
+    
     @Given("^I am on the home page$")
     public void i_am_on_the_home_page() throws Throwable {
         Driver.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().get("http://etsy.com");
-
+        
     }
-
+    
     @When("^I search for \"([^\"]*)\"$")
     public void i_search_for(String search) throws Throwable {
-        Driver.getDriver().findElement(By.xpath("//input[@id='global-enhancements-search-query']")).sendKeys(search + Keys.ENTER);
+        Driver.getDriver().findElement(By.cssSelector("[id*='search-query']")).sendKeys(search + Keys.ENTER);
     }
-
+    
     @Then("^I should see the results$")
     public void i_should_see_the_results() throws Throwable {
-        Thread.sleep(2000);
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
     }
-
+    
     @Then("^I should see more results$")
     public void i_should_see_more_results() throws Throwable {
-        Thread.sleep(2000);
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
     }
-
+    
     @After
     public void tearDown(Scenario scenario) {
         if (scenario.isFailed()) {
@@ -51,5 +50,5 @@ public class StepDefs {
         }
         Driver.closeDriver();
     }
-
+    
 }
